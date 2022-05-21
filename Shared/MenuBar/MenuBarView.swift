7b92b23f5007b325249ae3e426490b388d627c92
HanCh <05.hanch.it@gmail.com>
2022-05-21 18:46:14 +0900

    feat: menuBar 추가

---
+++ Shared/MenuBar/MenuBarView.swift
@@ -0,0 +1,20 @@
+//
+//  MenuBar.swift
+//  clipboard-manager (macOS)
+//
+//  Created by HanCh on 2022/05/21.
+//
+
+import SwiftUI
+
+struct MenuBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MenuBar_Previews: PreviewProvider {
+    static var previews: some View {
+        MenuBar()
+    }
+}
---
+++ Shared/StatusBar.swift
@@ -0,0 +1,20 @@
+//
+//  StatusBar.swift
+//  clipboard-manager (macOS)
+//
+//  Created by HanCh on 2022/05/21.
+//
+
+import SwiftUI
+
+struct StatusBar: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct StatusBar_Previews: PreviewProvider {
+    static var previews: some View {
+        StatusBar()
+    }
+}
